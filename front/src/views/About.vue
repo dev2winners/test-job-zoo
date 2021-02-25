6c542dcb0d610542fe53842612f851100ae7cf47
Merecoin <36365036+merecoin@users.noreply.github.com>
2021-02-25 13:14:25 +0300

    axios

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -2,5 +2,30 @@
   <div class="about">
     <h1>This is an about page</h1>
     <b-button variant="success">Button</b-button>
+    {{ coin }}
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      coin: '',
+    };
+  },
+  methods: {
+    getAxios() {
+      this.axios
+        //.get(process.env.VUE_APP_BACKEND_URL + '/')
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then((response) => (this.coin = response.data))
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  mounted() {
+    this.getAxios();
+  },
+};
+</script>
