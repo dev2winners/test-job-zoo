29573df21d2e2961906ca727822f5498dee686db
Merecoin <36365036+merecoin@users.noreply.github.com>
2021-02-25 13:47:56 +0300

    Zoo vue

---
+++ front/src/views/Zoo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>Наш Зоопарк!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      coin: '',
+    };
+  },
+  methods: {
+    getAxios() {
+      this.axios
+        //.get(process.env.VUE_APP_BACKEND_URL + '/')
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then((response) => (this.coin = response.data))
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  mounted() {},
+};
+</script>
